d0b9156b2669b68d245aebdb48bd4ebe4cca50f5
dunlopjo <joyce.dunlop@hpe.com>
2018-02-28 09:29:16 -0600

    Environment support

--- config/prodstage.env.js
+++ config/prodstage.env.js
@@ -3,4 +3,5 @@ module.exports = {
   FHIR_URL: '"https://fhir.sandbox.openplatform.healthcare/"',
   FHIR_PROCESS_URL: '"https://ohc-cache-refresh.sandbox.openplatform.healthcare"',
   EHR_URL: '"https://ehr.sandbox.openplatform.healthcare/"'
+  SYNTHEA_URL: '"https://synthea-service-sandbox.openplatform.healthcare/"'
 }
