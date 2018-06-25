e391e4621dff7d57e01c0d64fa02b3faea87af12
Rob Caruso <rob.caruso@dxc.com>
2018-06-25 09:50:12 -0400

    updated for opensource

--- config/prodstage.env.js
+++ config/prodstage.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production-stage"',
-  SYNTHEA_URL: '"https://synthea-service-sandbox.openplatform.healthcare/"',
+  SYNTHEA_URL: '"http://localhost:8021/"',
   // V2O_URL: '""' or missing for open source
-  V2O_URL: '"https://vista-to-ohc-sandbox.openplatform.healthcare/"'
+  V2O_URL: '""'
 }
